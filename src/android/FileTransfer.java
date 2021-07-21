97ddf2f04a281ce17262c0113737379fb9153065
Priyansu Prasad <priyansu.p@systenics.net>
2021-07-21 16:08:34 +0530

    Replaced WhiteList class with AllowList to resolve cordova-android-10

--- src/android/FileTransfer.java
+++ src/android/FileTransfer.java
@@ -45,7 +45,7 @@ import org.apache.cordova.CordovaResourceApi.OpenForReadResult;
 import org.apache.cordova.LOG;
 import org.apache.cordova.PluginManager;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.Whitelist;
+import org.apache.cordova.AllowList;
 import org.apache.cordova.file.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -688,8 +688,8 @@ public class FileTransfer extends CordovaPlugin {
         if (shouldAllowRequest == null) {
             try {
                 Method gwl = webView.getClass().getMethod("getWhitelist");
-                Whitelist whitelist = (Whitelist)gwl.invoke(webView);
-                shouldAllowRequest = whitelist.isUrlWhiteListed(source);
+                AllowList whitelist = (AllowList)gwl.invoke(webView);
+                shouldAllowRequest = whitelist.isUrlAllowListed(source);
             } catch (NoSuchMethodException e) {
             } catch (IllegalAccessException e) {
             } catch (InvocationTargetException e) {
