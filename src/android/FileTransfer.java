de6886bb2753bf775f2bb898ad6bc54c9ca5ca02
Priyansu Prasad <priyansu.p@systenics.net>
2021-07-23 22:53:39 +0530

    remove unwanted code

--- src/android/FileTransfer.java
+++ src/android/FileTransfer.java
@@ -912,16 +912,3 @@ public class FileTransfer extends CordovaPlugin {
         }
     }
 }
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
